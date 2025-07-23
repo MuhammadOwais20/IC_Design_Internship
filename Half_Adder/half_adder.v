ae132c3938131268c1b7631a29b56a1c1b6c37f9
Muhammad Owais <robinowais08@gmail.com>
2025-07-23 19:27:43 +0500

    Update half_adder.v

--- Half_Adder/half_adder.v
+++ Half_Adder/half_adder.v
@@ -1,3 +1,7 @@
+// MIT License
+// Copyright (c) 2025 Muhammad Owais
+// See the LICENSE file in the root of this project for full license information.
+
 module half_adder (
   input wire a,
   input wire b,
