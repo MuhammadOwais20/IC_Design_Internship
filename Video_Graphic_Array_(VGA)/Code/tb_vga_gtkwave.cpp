2aef2864f47b3dbd43644bf1fcff24f9102d033e
Muhammad Owais <robinowais08@gmail.com>
2025-08-10 23:49:31 +0500

    Update tb_vga_gtkwave.cpp

--- Video_Graphic_Array_(VGA)/Code/tb_vga_gtkwave.cpp
+++ Video_Graphic_Array_(VGA)/Code/tb_vga_gtkwave.cpp
@@ -1,41 +1,49 @@
-#include "verilated.h"
+#include <verilated.h>
 #include "verilated_vcd_c.h"
-#include "Vvga_controller_800x600.h"
+#include "Vvga_controller.h"
 
+Vvga_controller* top;
 vluint64_t main_time = 0;
+
 double sc_time_stamp() { return main_time; }
 
-int main() {
-    int argc = 1;
-    const char* argv[] = {"tb_vga_gtkwave"};
-    Verilated::commandArgs(argc, argv);
-    Verilated::traceEverOn(true);
+uint16_t generate_pixel_data(uint16_t v) {
+    if (v < 150) return 0xF800; // Red
+    if (v < 300) return 0x07E0; // Green
+    if (v < 450) return 0x001F; // Blue
+    return 0xFFFF;              // White
+}
 
-    Vvga_controller_800x600* top = new Vvga_controller_800x600;
+int main() {
+    Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
+    top = new Vvga_controller;
+
     top->trace(tfp, 99);
     tfp->open("vga.vcd");
 
-    // Apply reset
+    uint16_t sram[800 * 600] = {0};
+
+    // Reset
+    top->clk = 0;
     top->reset = 1;
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; i++) {
         top->clk = 0; top->eval(); tfp->dump(main_time++);
         top->clk = 1; top->eval(); tfp->dump(main_time++);
     }
     top->reset = 0;
 
-    // Main simulation loop
-    while (main_time < 20'000'000) {
-        top->clk = 0; top->eval(); tfp->dump(main_time++);
+    while (!Verilated::gotFinish() && main_time < 2'000'000) {
+        top->clk = 0;
+        top->eval();
+        tfp->dump(main_time++);
 
-        uint16_t v = top->vc;
-        uint16_t sram_data = (v < 150) ? 0xF800 :
-                             (v < 300) ? 0x07E0 :
-                             (v < 450) ? 0x001F : 0xFFFF;
+        sram[top->sram_address] = generate_pixel_data(top->vc);
+        top->sram_data = sram[top->sram_address];
 
-        top->sram_data = sram_data;
-
-        top->clk = 1; top->eval(); tfp->dump(main_time++);
+        top->clk = 1;
+        top->eval();
+        tfp->dump(main_time++);
     }
 
     tfp->close();
@@ -43,4 +51,3 @@ int main() {
     delete top;
     return 0;
 }
-
